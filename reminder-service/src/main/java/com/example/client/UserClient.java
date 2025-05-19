06dfd51ef5e9bb87fbb328b870cdad9e80bbd585
omaarrwaell <omarwael33@gmail.com>
2025-05-19 23:12:15 +0300

    Final

--- reminder-service/src/main/java/com/example/client/UserClient.java
+++ reminder-service/src/main/java/com/example/client/UserClient.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "user-service", url = "${user.service.url}",configuration = UserClientConfig.class)
+@FeignClient(name = "user-service",configuration = UserClientConfig.class)
 public interface UserClient {
     @GetMapping("/api/users/{id}")
     UserDto getUser(@PathVariable("id") String id);
